16e0d893adcec91566e486e318c50822371e52f8
Dipto-Datta <diptodattag@gmail.com>
2024-01-20 23:30:06 +0600

    changed repo : update

---
+++ src/components/topbarView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="z-10">
+    <div
+      class="h-[100px] w-full bg-gray-100 grid grid-cols-7 items-center text-black px-5"
+    >
+      <div class="">Territory Runner TM</div>
+      <div class="flex gap-2 items-center justify-center col-span-5">
+        <input
+          type="text"
+          placeholder="Location Search"
+          class="h-9 p-3 border-1 border-black rounded-lg"
+        />
+        <dropdown title="Contains"></dropdown>
+        <checkbox name="Place"></checkbox>
+        <checkbox name="Data"></checkbox>
+        <checkbox name="Layer"></checkbox>
+        <checkbox name="Territory"></checkbox>
+        <button class="bg-buttonColor rounded-lg py-2 px-3 text-white">
+          Search
+        </button>
+        <button class="bg-buttonColor rounded-lg py-2 px-3 text-white">
+          Reset
+        </button>
+        <div class="border-l-2 border-gray-600 pl-3 space-x-2">
+          <button class="bg-buttonColor rounded-lg py-2 px-3 text-white">
+            Demographics
+          </button>
+          <button class="bg-buttonColor rounded-lg py-2 px-3 text-white">
+            Re-Set
+          </button>
+        </div>
+      </div>
+      <div class="flex justify-end">
+        <div class="bg-green-600 rounded-lg py-2 px-3 w-3/5 text-white">
+          User Name
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import checkbox from "@/components/widget/checkboxView.vue";
+import dropdown from "@/components/widget/dropdownView.vue";
+export default {
+  components: {
+    checkbox,
+    dropdown,
+  },
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <topbar class="z-10"></topbar>
+    <div>
+      <l-map
+        style="height: calc(100vh - 100px)"
+        ref="map"
+        v-model:zoom="zoom"
+        :center="[47.41322, -1.219482]"
+      >
+        <l-tile-layer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          layer-type="base"
+          name="OpenStreetMap"
+        ></l-tile-layer>
+      </l-map>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+import topbar from "@/components/topbarView.vue";
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+
+export default {
+  name: "HomeView",
+  components: {
+    topbar,
+    LMap,
+    LTileLayer,
+  },
+  setup() {
+    const zoom = ref(10);
+
+    onMounted(() => {});
+
+    return {
+      zoom,
+    };
+  },
+};
+</script>
